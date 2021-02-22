ef1de0f8dd563e9c617e2c6d1f72e6f0ccf6f358
Tom de Geus <tom@geus.me>
2021-02-22 15:21:33 +0100

    GooseFEM: properly converting type in checks

--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,9 @@
 #define CATCH_CONFIG_MAIN  // tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
 #include <GooseFEM/GooseFEM.h>
+#include <HighFive/H5Easy.hpp>
+
+#define XDMFWRITE_HIGHFIVE_INDENT 0
 #include <XDMFWrite_HighFive.hpp>
 
 namespace xh = XDMFWrite_HighFive;
@@ -14,7 +17,7 @@ TEST_CASE("XDMFWrite_HighFive", "Basic")
         std::cout << xh::version() << std::endl;
     }
 
-    SECTION("GooseFEM")
+    SECTION("GooseFEM - detail")
     {
         {
             auto t = GooseFEM::Mesh::ElementType::Tri3;
@@ -28,6 +31,29 @@ TEST_CASE("XDMFWrite_HighFive", "Basic")
             auto t = GooseFEM::Mesh::ElementType::Hex8;
             REQUIRE(xh::detail::to<decltype(t)>::str(t) == "Hexahedron");
         }
+    }
+
+    SECTION("GooseFEM - mesh")
+    {
+        H5Easy::File file("tmp.h5", H5Easy::File::Overwrite);
+
+        GooseFEM::Mesh::Quad4::Regular mesh(3, 3);
+
+        H5Easy::dump(file, "/coor", mesh.coor());
+        H5Easy::dump(file, "/conn", mesh.conn());
+
+        std::vector<std::string> t = {
+            "<Topology NumberOfElements=\"9\" TopologyType=\"Quadrilateral\">",
+            "<DataItem Dimensions=\"9 4\" Format=\"HDF\">tmp.h5:/conn</DataItem>",
+            "</Topology>"};
+
+        std::vector<std::string> g = {
+            "<Geometry GeometryType=\"XY\">",
+            "<DataItem Dimensions=\"16 2\" Format=\"HDF\">tmp.h5:/coor</DataItem>",
+            "</Geometry>"};
+
+        REQUIRE(t == xh::Topology(file, "/conn", mesh.getElementType()));
+        REQUIRE(g == xh::Geometry(file, "/coor"));
     }
 }
 
