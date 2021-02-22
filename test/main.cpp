da86d8402c944ac7b279ec490129ce0f26845099
Tom de Geus <tom@geus.me>
2021-02-22 11:04:04 +0100

    [CI] Debugging windows

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,34 @@
 
 #define CATCH_CONFIG_MAIN  // tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
+#include <GooseFEM/GooseFEM.h>
 #include <XDMFWrite_HighFive.hpp>
+
+namespace xh = XDMFWrite_HighFive;
 
 TEST_CASE("XDMFWrite_HighFive", "Basic")
 {
 
     SECTION("version")
     {
-        std::cout << XDMFWrite_HighFive::version() << std::endl;
+        std::cout << xh::version() << std::endl;
     }
 
+    SECTION("GooseFEM")
+    {
+        {
+            auto t = GooseFEM::Mesh::ElementType::Tri3;
+            REQUIRE(xh::detail::to<decltype(t)>::str(t) == "Triangle");
+        }
+        {
+            auto t = GooseFEM::Mesh::ElementType::Quad4;
+            REQUIRE(xh::detail::to<decltype(t)>::str(t) == "Quadrilateral");
+        }
+        {
+            auto t = GooseFEM::Mesh::ElementType::Hex8;
+            REQUIRE(xh::detail::to<decltype(t)>::str(t) == "Hexahedron");
+        }
+    }
 }
 
 
